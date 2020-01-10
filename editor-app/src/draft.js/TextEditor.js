434a44ce80076cde823260a82ffe705f8682aec9
chrispetito <christopherpetito@gmail.com>
2020-01-09 18:35:27 -0800

    add draft plugins

--- editor-app/src/draft.js/TextEditor.js
+++ editor-app/src/draft.js/TextEditor.js
@@ -1,108 +1,41 @@
-import React from "react";
-import { Editor, EditorState, RichUtils } from "draft-js";
-import "../css/index.css";
+import React, { Component } from 'react';
+import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
+import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin';
+import editorStyles from '../css/editorStyles.css'
+import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
 
-class TextEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { editorState: EditorState.createEmpty() };
-    this.onChange = editorState => this.setState({ editorState });
-    this.setEditor = editor => {
-      this.editor = editor;
-    };
-    console.log(this.state.editorState);
-    this.focusEditor = () => {
-      if (this.editor) {
-        this.editor.focus();
-      }
-    };
-  }
+const inlineToolbarPlugin = createInlineToolbarPlugin();
+const { InlineToolbar } = inlineToolbarPlugin;
+const plugins = [inlineToolbarPlugin];
+const text = 'In this editor a toolbar shows up once you select part of the text …';
 
-  boldOnClick = () => {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "BOLD"));
+export default class TextEditor extends Component {
+
+  state = {
+    editorState: createEditorStateWithText(text),
   };
 
-  italicOnClick = () => {
-    this.onChange(
-      RichUtils.toggleInlineStyle(this.state.editorState, "ITALIC")
-    );
+  onChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
   };
 
-  underlineOnClick = () => {
-    this.onChange(
-      RichUtils.toggleInlineStyle(this.state.editorState, "UNDERLINE")
-    );
-  };
-  handleKeyCommand(command, editorState) {
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return "handled";
-    }
-    return "not-handled";
-  }
-
-  //   publishPost = () => {
-  //     // turn the state to html
-  //     const html = convertToHTML(this.state.editorState.getCurrentContent());
-
-  //     const post = {
-  //         content: html,
-  //         createdAt: new Date()
-  //         // more stuff...
-  //     }
-
-  //     // post the data to you mongo storage.
-  // }
-
-  componentDidMount() {
-    this.focusEditor();
-  }
-
-  blockStyleFn = contentBlock => {
-    const type = contentBlock.getType();
-    if (type === "blockquote") {
-      return "superFancyBlockquote";
-    }
+  focus = () => {
+    this.editor.focus();
   };
 
   render() {
     return (
-        <div className='editor-div'>
-        <div className="button-bar">
-        {" "}
-        <button className="bold-button" onClick={this.boldOnClick}>
-          <i class="fas fa-bold"></i>
-        </button>
-        <button onClick={this.italicOnClick}>
-          <i class="fas fa-italic"></i>
-        </button>
-        <button onClick={this.underlineOnClick}>
-          <i class="fas fa-underline"></i>
-        </button>
-      </div>
-      <div style={styles.editor} onClick={this.focusEditor} className='text-editor'>
+      <div className={editorStyles.editor} onClick={this.focus}>
         <Editor
-          //   ref={this.setEditor}
           editorState={this.state.editorState}
-          handleKeyCommand={this.handleKeyCommand}
           onChange={this.onChange}
-          blockStyleFn={this.blockStyleFn}
+          plugins={plugins}
+          ref={(element) => { this.editor = element; }}
         />
-      </div>
+        <InlineToolbar />
       </div>
     );
   }
 }
-
-const styles = {
-  editor: {
-    border: "1px solid gray",
-    minHeight: "10em",
-    // width: "50%",
-    textAlign: "left",
-    padding: 12
-  }
-};
-
-export default TextEditor;
